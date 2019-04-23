35905758319c864eb6e8e6967d801d729b7a23c6
Michael Allwright <allsey87@gmail.com>
2019-04-23 18:12:16 +0200

    Add a simple stack-based container

---
+++ firmware/source/utils/container.h
@@ -0,0 +1,49 @@
+#ifndef CONTAINER_H
+#define CONTAINER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+void* operator new (size_t n, void* ptr) {
+   return ptr;
+};
+
+template <typename T, size_t length>
+class container {
+public:
+
+   ~container() {
+      for(T& t : *this) t.~T();
+   }
+
+   T* begin() {
+      return reinterpret_cast<T*>(data);
+   }
+
+   T* end() {
+      return begin() + used_count;
+   }
+
+   template<typename... Arguments>
+   T* insert(const Arguments&... arguments) {
+      if(used_count < length) {
+         T* created_instance = new (end()) T(arguments...);
+         ++used_count;
+         return created_instance;
+      }
+      else {
+         return nullptr;
+      }
+   }
+
+   size_t size() {
+      return used_count;
+   }
+
+private:
+   unsigned char data[length * sizeof(T)];
+   size_t used_count = 0;
+   
+};
+
+#endif
